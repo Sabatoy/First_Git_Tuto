b350143a064764acc2ff00440c7d0ea08cab6532
Yves Sabato <ysabato@sessionm.com>
2015-01-25 16:05:47 -0500

    new .rb text

---
+++ sec_tuto.rb
@@ -0,0 +1,15 @@
+# class Sabato
+#   def first_time()
+#     puts "Hello punk"
+#   end
+# end
+# 
+# 
+# yves = Sabato.new
+# yves.first_time
+# 
+# puts yves.class
+
+title = gets.chomp
+
+puts title
